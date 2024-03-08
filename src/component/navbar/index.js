bdf4318ba7f7cb25cffff1f58c2d1c16fdd1d15f
usamajaved4216 <usamajaved4216@gmail.com>
2024-03-09 02:43:35 +0500

    navbar responsive

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,43 +1,41 @@
-import { RxHamburgerMenu } from "react-icons/rx";
-import { IoClose } from "react-icons/io5";
-import { useState } from "react";
+import { useRef} from "react";
 import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from "react-icons/fa";
+
 
 const Navbar = () => {
-  const [sidebarVisible, setSidebarVisible] = useState(false);
 
-  const showSidebar = () => {
-    setSidebarVisible(true);
-  };
-
-  const hideSidebar = () => {
-    setSidebarVisible(false);
-  };
+  const navRef = useRef();
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
 
   return (
-    <div style={{height:'10vh'}}>
-      <nav>
-        <ul className={`sidebar ${sidebarVisible ? 'visible' : ''}`}>
-          <li className="menu-button" onClick={hideSidebar}><a href="#"><IoClose fontSize={30} /></a></li>
+    <>
+    <header>
+			<h3 style={{ color: ' #bf2237f1', fontWeight: 'bold', fontSize: '25px',}}>Usama Javed</h3>
+			<nav ref={navRef}>
+      <Link to="home" smooth={true} duration={500}><a>Home</a></Link>
+      <Link to="about" smooth={true} duration={500}><a>About</a></Link>
+			<Link to="service" smooth={true} duration={500}><a>Service</a></Link>
+      <Link to="project" smooth={true} duration={500}><a>Project</a></Link>
+      <Link to="contact" smooth={true} duration={500}><a>Contact</a></Link>
 
-          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-          <li><Link to="service" smooth={true} duration={500}>Service</Link></li>
-          <li><Link to="project" smooth={true} duration={500}>Project</Link></li>
-          <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
-        </ul>
-        <ul>
-          <li><Link to="home" smooth={true} duration={500}><a style={{ color: ' #bf2237f1', fontWeight: 'bold', fontSize: '25px',}}>Usama Javed</a></Link></li>
-          <li className="hideOnMobile"><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li className="hideOnMobile"><Link to="about" smooth={true} duration={500}>About</Link></li>
-          <li className="hideOnMobile"><Link to="service" smooth={true} duration={500}>Service</Link></li>
-          <li className="hideOnMobile"><Link to="project" smooth={true} duration={500}>Project</Link></li>
-          <li className="hideOnMobile"><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
-          <li className="menu-button" onClick={showSidebar}><a href="#"><RxHamburgerMenu fontSize={30} /></a></li>
-        </ul>
-
-      </nav>
-    </div>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+    </>
   );
 };
 
--- src/views/contact/index.js
+++ src/views/contact/index.js
@@ -5,13 +5,13 @@ function Contact() {
     <>
     
     <div id='contact' style={{ height:'100%', textAlign: 'center', padding: '50px', backgroundColor: 'bisque', borderRadius: '10px', }}>
-      <h1 style={{color:'black', fontSize:"50px"}}>CONTACT</h1>
-      <p style={{ marginBottom: '30px', color: '#333', fontSize:"30px" }}>Have a question or want to work together?</p>
+      <h1 style={{color:'black', fontSize:"40px"}}>CONTACT</h1>
+      <p className='contactDescrpition'>Have a question or want to work together?</p>
       <div style={{ margin: '30px 0' }}>
-        <p style={{ marginBottom: '10px', fontSize: '18px', lineHeight: '1.5', color: 'black', fontSize:'25px', fontWeight:'bold' }}>Email: <a href="https://mail.google.com/mail/u/0/?ogbl#inbox" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>usamajaved4216@gmail.com</a></p>
-        <p style={{ marginBottom: '10px', fontSize: '18px', lineHeight: '1.5', color: 'black', fontSize:'25px', fontWeight:'bold' }}>WhatsApp: <a href="https://web.whatsapp.com/" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>+92 319 6561816</a></p>
-        <p style={{ marginBottom: '10px', fontSize: '18px', lineHeight: '1.5', color: 'black', fontSize:'25px', fontWeight:'bold' }}>LinkedIn: <a href="https://www.linkedin.com/in/usama-javed-8ba27829b/" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>Usama Javed</a></p>
-        <p style={{ marginBottom: '10px', fontSize: '18px', lineHeight: '1.5', color: 'black', fontSize:'25px', fontWeight:'bold' }}>Instagram: <a href="https://www.instagram.com/usamasheikh4216/" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>usamajaved4216</a></p>
+        <p className='contactfont' >Email: <a href="https://mail.google.com/mail/u/0/?ogbl#inbox" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>usamajaved4216@gmail.com</a></p>
+        <p className='contactfont' >WhatsApp: <a href="https://web.whatsapp.com/" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>+92 319 6561816</a></p>
+        <p className='contactfont' >LinkedIn: <a href="https://www.linkedin.com/in/usama-javed-8ba27829b/" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>Usama Javed</a></p>
+        <p className='contactfont' >Instagram: <a href="https://www.instagram.com/usamasheikh4216/" style={{ color: '#0088cc', textDecoration: 'none', fontWeight: 'bold' }}>usamajaved4216</a></p>
       </div>
       <p style={{ fontSize: '16px', color: '#333', fontSize:'25px' }}>Made with ❤️ by Usama Javed</p>
     </div>
